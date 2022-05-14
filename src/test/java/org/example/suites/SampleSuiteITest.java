ff854bb5ad4b9bbd68dc71bf1725bac642bd7cac
Ghouseak <ghouseak@gmail.com>
2022-05-15 02:10:22 +0530

    initial commit

---
+++ src/test/java/org/example/suites/SampleSuiteITest.java
@@ -0,0 +1,11 @@
+package org.example.suites;
+
+import org.testng.annotations.Test;
+
+public class SampleSuiteITest {
+  @Test
+  public void test() {
+    System.out.println("Test passed");
+  }
+
+}
