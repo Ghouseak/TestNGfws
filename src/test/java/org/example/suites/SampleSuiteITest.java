080f8e219f90bd4fd2a8b4893af30a5f088afd52
Ghouseak <ghouseak@gmail.com>
2022-05-15 22:17:04 +0530

    cd pom samplesuit java

--- src/test/java/org/example/suites/SampleSuiteITest.java
+++ src/test/java/org/example/suites/SampleSuiteITest.java
@@ -1,10 +1,17 @@
 package org.example.suites;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class SampleSuiteITest {
   @Test
   public void test() {
+    System.setProperty("webdriver.chrome.driver", "C:\\Users\\Ali\\IdeaProjects\\TestNGfws\\src\\test\\resources\\chromedriver.exe");
+
+    // Instantiate a ChromeDriver class.
+    WebDriver driver=new ChromeDriver();
+    driver.get("https://www.google.com/");
     System.out.println("Test passed");
   }
 
